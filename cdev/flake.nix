92d20515a8b1b306ecf60bd9afdcea2cdff0cb7b
gratisvictory <alex.borzov@proton.me>
2025-03-12 21:07:29 +0400

    feat(nix): add flake for c-dev

---
+++ cdev/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "My C Project with libcheck";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          check
+          pkg-config
+          gcc
+          gdb
+          gnumake
+          valgrind
+          lcov
+          clang-tools
+          nushell
+          subunit
+          firefox
+        ];
+        shellHook = ''
+          nu
+        '';
+      };
+    });
+}
